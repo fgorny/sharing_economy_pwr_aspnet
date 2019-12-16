8849fadf2c6be6b37238692b95e3e5753952dd47
majsterJelenia <58252256+majsterJelenia@users.noreply.github.com>
2019-12-16 16:11:39 +0100

    rezerwacja prod v1

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -12,12 +12,12 @@ namespace ProductsApp.Controllers
         Product[] products = new Product[]
         {
             new Product { Id = 1, Name = "Zupa pomidorowa", Category = "Groceries", Price = 1 },
-            new Product { Id = 2, Name = "Herbata", Category = "Toys", Price = 3.75M },
-            new Product { Id = 3, Name = "Jeansy rozmiar 38", Category = "Hardware", Price = 16.99M },
-            new Product { Id = 4, Name = "Zabawka pluszowa dinozaur", Category = "Hardware", Price = 16.99M },
-            new Product { Id = 5, Name = "Kurtka Adidas M", Category = "Hardware", Price = 16.99M },
-            new Product { Id = 6, Name = "Obiad raz w tygodniu", Category = "Hardware", Price = 16.99M },
-            new Product { Id = 7, Name = "Pomoc w nauce", Category = "Hardware", Price = 16.99M }
+            new Product { Id = 2, Name = "Herbata", Category = "Toys", Price = 0.50M },
+            new Product { Id = 3, Name = "Jeansy rozmiar 38", Category = "Hardware", Price = 10M },
+            new Product { Id = 4, Name = "Zabawka pluszowa dinozaur", Category = "Hardware", Price = 2 },
+            new Product { Id = 5, Name = "Kurtka Adidas M", Category = "Hardware", Price = 10M },
+            new Product { Id = 6, Name = "Obiad raz w tygodniu", Category = "Hardware", Price = 5M },
+            new Product { Id = 7, Name = "Pomoc w nauce", Category = "Hardware", Price = 10M }
         };
 
         public IEnumerable<Product> GetAllProducts()
